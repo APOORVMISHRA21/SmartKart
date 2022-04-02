1acfec61ce9ed3a7ec67a86bc8a274c546b3a84b
APOORVMISHRA21 <443_bt19@iiitkalyani.ac.in>
2022-04-02 14:57:30 +0530

    SplashActivity

---
+++ app/src/main/java/com/example/smartkart/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartkart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
